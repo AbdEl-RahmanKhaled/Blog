c7a28dd6837bb86adc9dafadc5255a869dd22c0d
AbdEl-Rahman <3bdo.kha@gmail.com>
2022-03-18 15:54:41 +0200

    update models

--- Blog/urls.py
+++ Blog/urls.py
@@ -19,8 +19,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('account/', include('Accounts.urls')),
-    path('', include('Posts.urls')),
+                  path('admin/', admin.site.urls),
+                  path('account/', include('Accounts.urls')),
+                  path('', include('Posts.urls')),
 
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Posts/models.py
+++ Posts/models.py
@@ -4,12 +4,19 @@ from django.db import models
 class Category(models.Model):
     category = models.CharField(max_length=30, blank=False, null=False, verbose_name='Category')
 
+    def __str__(self):
+        return self.category
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100, blank=False, verbose_name='Title')
+    date = models.DateTimeField(auto_now_add=True, verbose_name='Post Date')
     pic = models.ImageField(upload_to='photos/%Y/%M/%D', verbose_name='Post Picture')
-    content = models.CharField(max_length=200, blank=False, verbose_name='Post Content')
+    content = models.TextField(max_length=200, blank=False, verbose_name='Post Content')
     category = models.ForeignKey(Category, on_delete=models.SET_DEFAULT, default='undefined',
                                  related_name='posts_related')
     likes = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
