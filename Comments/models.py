a9779b1b497026502f7a0a41046105f28153fdb8
AbdEl-Rahman <3bdo.kha@gmail.com>
2022-03-18 15:31:11 +0200

    update comment model

--- Comments/models.py
+++ Comments/models.py
@@ -1,15 +1,14 @@
 from django.db import models
-import Accounts.models
-import Posts.models
-import Posts.models
+from Accounts.models import Account
+from Posts.models import Post
 
 
 # Create your models here.
 
 
 class Comments(models.Model):
-    post = models.ForeignKey(Posts.models.Post, on_delete=models.CASCADE, related_name='Comments')
-    user = models.ForeignKey(Accounts.models.Account, on_delete=models.CASCADE, default='Unknown',
-                             related_name='Comment Owner')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='Comments')
+    user = models.ForeignKey(Account, on_delete=models.CASCADE, default='Unknown',
+                             related_name='comment_owner')
     content = models.TextField(verbose_name='Comment Content')
     created_on = models.DateTimeField(auto_now_add=True, verbose_name='Comment Time')
