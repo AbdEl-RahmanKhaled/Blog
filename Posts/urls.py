f113874542939f6acdf937344892b919601d3898
AbdEl-Rahman <3bdo.kha@gmail.com>
2022-03-23 19:20:32 +0200

    update urls

--- Posts/urls.py
+++ Posts/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('timeline', views.posts, name='posts'),
-    path('Post-detail/<p_id>', views.post_detail, name='postDetails'),
-    path('Likes', views.like, name ="like"),
-    path('dislikes', views.dislikes, name ="dislikes"),
-    path('sub-category/<cat_id>', views.sub_category, name="sub_category"),
-    path('unsub-category/<cat_id>', views.unsub_category, name="unsub_category"),
+    path('posts/timeline', views.posts, name='posts'),
+    path('posts/Post-detail/<p_id>', views.post_detail, name='postDetails'),
+    path('posts/Likes', views.like, name="like"),
+    path('posts/dislikes', views.dislikes, name="dislikes"),
+    path('posts/sub-category/<cat_id>', views.sub_category, name="sub_category"),
+    path('posts/unsub-category/<cat_id>', views.unsub_category, name="unsub_category"),
 ]
