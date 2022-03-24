e270a398b64290a30f2407a467d15d76c3112120
AbdEl-Rahman <3bdo.kha@gmail.com>
2022-03-24 20:35:58 +0200

    fix sidebar on scrolling

--- templates/staticfiles/js/main.js
+++ templates/staticfiles/js/main.js
@@ -1,2 +1,16 @@
-const date = new Date();
-document.querySelector('.year').innerHTML = date.getFullYear();
+set_new_pass = document.getElementById('set_new_pass');
+set_new_confirm_pass = document.getElementById('set_new_confirm_pass');
+btn_set_new_pass = document.getElementById('btn_set_new_pass');
+
+
+function is_valid() {
+    let pass1 = set_new_pass.value;
+    let pass2 = set_new_confirm_pass.value;
+    return pass1 === pass2
+}
+
+btn_set_new_pass.addEventListener('click', function (e) {
+    if (!is_valid()) {
+        e.preventDefault();
+    }
+});
