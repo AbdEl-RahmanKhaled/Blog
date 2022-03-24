3cb88839681c5e264b5192cfb795acf2aa1196b6
AbdEl-Rahman <3bdo.kha@gmail.com>
2022-03-24 20:39:36 +0200

    update urls

--- Posts/urls.py
+++ Posts/urls.py
@@ -2,13 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('timeline', views.posts, name ='Posts'),
-    path('Post_detail/<p_id>', views.post_detail, name='postDetails'),
-    path('Likes', views.like, name ="like"),
-    path('dislikes', views.dislikes, name ="dislikes"),
-    path('sub_category/<cat_id>', views.sub_category, name="sub_category"),
-    path('unsub_category/<cat_id>', views.unsub_category, name="unsub_category"),
-    path('admin-posts/', views.PostsAdminView.as_view(), name="admin-posts"),
-    path('admin-editPost/<p_id>', views.PostEditAdmin.as_view(), name="adminEditPost")
+    path('', views.posts, name ='index'),
+    path('posts/Post_detail/<p_id>', views.post_detail, name='postDetails'),
+    path('posts/Likes', views.like, name ="like"),
+    path('posts/dislikes', views.dislikes, name ="dislikes"),
+    path('posts/sub_category/<cat_id>', views.sub_category, name="sub_category"),
+    path('posts/unsub_category/<cat_id>', views.unsub_category, name="unsub_category"),
+    path('posts/admin-posts', views.PostsAdminView.as_view(), name="admin-posts"),
+    path('posts/admin-editPost/<p_id>', views.PostEditAdmin.as_view(), name="adminEditPost")
 ]
