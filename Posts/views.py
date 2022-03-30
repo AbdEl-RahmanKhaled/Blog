7d200e6127bdf59ec01f21e3c6d90f34c4d690fb
AbdEl-RahmanKhaled <a.kha.anw@gmail.com>
2022-03-30 13:39:09 +0200

    fix pugs

--- Posts/views.py
+++ Posts/views.py
@@ -6,6 +6,7 @@ from Posts.models import Post, PostLikes, PostDislikes, Category
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from Helpers.Decorators.auth_decorators import verified_acc_only
+from django.db.models import Q
 
 
 def index(request):
@@ -14,34 +15,26 @@ def index(request):
 
 def posts(request):
     post = Post.objects.all().order_by('-date')
-    unsubbed_cat = Category.objects.all()
-    subbed_cat = []
+    subbed_unsubbed = get_cat(request)
     if request.user.is_authenticated:
-        subbed_cat = Account.objects.get(pk=request.user.id).accounts.all()
-        post = Post.objects.filter(category__in=list(subbed_cat.values_list('id', flat=True)))
-        unsubbed_cat = Category.objects.exclude(category__in=list(subbed_cat.values_list('category', flat=True)))
+        post = Post.objects.filter(category__in=list(subbed_unsubbed["subbed_cat"].values_list('id', flat=True)))
 
     paginator = Paginator(post, 5)
     page = request.GET.get('page')
     page_posts = paginator.get_page(page)
-    context = {"posts": page_posts,
-               "subbed_cat": subbed_cat,
-               "unsubbed_cat": unsubbed_cat}
+    context = {"posts": page_posts}
+    context.update(subbed_unsubbed)
     return render(request, 'posts/timeline.html', context)
 
 
 def post_detail(request, p_id):
     post = Post.objects.get(id=p_id)
     comments = Comment.objects.filter(post=post).order_by('-created_on')
-    subbed_cat = Account.objects.get(pk=request.user.id).accounts.all()
-    unsubbed_cat = Category.objects.exclude(category__in=list(subbed_cat.values_list('category', flat=True)))
-
+    subbed_unsubbed = get_cat(request)
     context = {
         'post': post,
-        'comments': comments,
-        "subbed_cat": subbed_cat,
-        "unsubbed_cat": unsubbed_cat
-    }
+        'comments': comments}
+    context.update(subbed_unsubbed)
     return render(request, 'posts/Post_detail.html', context)
 
 
@@ -111,9 +104,24 @@ def unsub_category(request, cat_id):
 
 def search(request):
     searched = request.GET['searched']
-    post = Post.objects.filter(title__icontains=searched)
+    post = Post.objects.filter(Q(title__icontains=searched) | Q(category__category__icontains=searched))
     paginator = Paginator(post, 5)
     page = request.GET.get('page')
     page_posts = paginator.get_page(page)
+
+    subbed_unsubbed = get_cat(request)
+
     context = {"posts": page_posts}
+    context.update(subbed_unsubbed)
     return render(request, 'posts/search_result.html', context)
+
+
+def get_cat(request):
+    unsubbed_cat = Category.objects.all()
+    subbed_cat = []
+    if request.user.is_authenticated:
+        subbed_cat = Account.objects.get(pk=request.user.id).accounts.all()
+        unsubbed_cat = Category.objects.exclude(category__in=list(subbed_cat.values_list('category', flat=True)))
+
+    return {"subbed_cat": subbed_cat,
+            "unsubbed_cat": unsubbed_cat}
